0c357c21418fbe20de468cc1a80f75405b3c603c
tbernard4 <tbernard@udesa.edu.ar>
2025-11-03 11:19:45 -0300

    carpeta components y screens (proyecto base listo)

---
+++ Prog3Grupo1/src/components/index.js
@@ -0,0 +1,2 @@
+import React, { Component } from 'react';
+import { View, Text, TextInput, Pressable, StyleSheet } from 'react-native';
---
+++ Prog3Grupo1/src/screens/indexSceen.js
@@ -0,0 +1,2 @@
+import React from 'react';
+import { View, Text, ScrollView, StyleSheet } from 'react-native';
